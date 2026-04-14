adfaaa48b17f06f67b74a76e818d8cad4c6a8da4
Anshul Kumar <anshulkumarchauhan25@gmail.com>
2026-04-15 02:38:28 +0530

    Checked the available embedding models

--- check_models.py
+++ check_models.py
@@ -1,16 +1,31 @@
+# import os
+# import google.generativeai as genai
+# from dotenv import load_dotenv
+
+# # .env se key load karein
+# load_dotenv()
+
+# # GenAI configure karein
+# api_key = os.getenv("GEMINI_API_KEY")
+# genai.configure(api_key=api_key)
+
+# print("Aapke liye available models ki list:\n")
+# # Sabhi models ko list karein jo text generation support karte hain
+# for m in genai.list_models():
+#     if 'generateContent' in m.supported_generation_methods:
+#         print(m.name)
+
+
+
 import os
 import google.generativeai as genai
 from dotenv import load_dotenv
 
-# .env se key load karein
 load_dotenv()
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
-# GenAI configure karein
-api_key = os.getenv("GEMINI_API_KEY")
-genai.configure(api_key=api_key)
-
-print("Aapke liye available models ki list:\n")
-# Sabhi models ko list karein jo text generation support karte hain
+print("Available Embedding models ki list:\n")
 for m in genai.list_models():
-    if 'generateContent' in m.supported_generation_methods:
+    # Is baar hum embedContent support check kar rahe hain
+    if 'embedContent' in m.supported_generation_methods:
         print(m.name)
