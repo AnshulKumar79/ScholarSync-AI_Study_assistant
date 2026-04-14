ce755f2e5aaa0b3dd621365221d581bfaab23665
Anshul Kumar <anshulkumarchauhan25@gmail.com>
2026-04-15 02:41:52 +0530

    Added endpoint for asking query

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import os
 import shutil
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
-from rag_utils import process_and_store_pdf
+from rag_utils import process_and_store_pdf, get_context_from_db
+from pydantic import BaseModel
+
+class QuestionRequest(BaseModel):
+    question: str
 
 #loading secret keys from .env file
 load_dotenv()
@@ -52,6 +56,41 @@ async def upload_document(file: UploadFile = File(...)):
             "status": "success",
             "message": f"File '{file.filename}' processed successfully.",
             "chunks_created": num_chunks
+        }
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
+
+
+
+@app.post("/ask")
+async def ask_question(req: QuestionRequest):
+    try:
+        #Database se relevant context nikalna
+        context = get_context_from_db(req.question)
+        
+        if "ERROR" in context:
+            return {"status": "error", "message": "Please upload a PDF first before asking questions."}
+
+        #AI ke liye ek strict Prompt banana
+        prompt = f"""You are an intelligent study assistant named ScholarSync. 
+        Please answer the user's question based ONLY on the provided Context. 
+        If the answer is not available in the context, clearly state: "I'm sorry, but the answer is not present in the uploaded document." Do not guess the answer.
+
+        Context:
+        {context}
+
+        Question:
+        {req.question}
+
+        Answer:"""
+
+        #Gemini ko prompt bhej kar answer lena
+        response = llm.invoke(prompt)
+
+        return {
+            "status": "success",
+            "question": req.question,
+            "answer": response.content
         }
     except Exception as e:
         return {"status": "error", "message": str(e)}
